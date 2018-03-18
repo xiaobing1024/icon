ea7d1299c086989d52b842753fcd4419f3dafe66
zxb <1030075254@qq.com>
2018-03-18 20:00:48 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,29 +14,7 @@ class DatabaseSeeder extends Seeder
 //        if (!App::isLocal()) {
 //            exit('你不会是想被开除吧 ?');
 //        }
-//        new Vue({
-//        el: '.asd',
-//  data: {
-//            text: 'Hello Vue!',
-//    backgroundColor: '#FF0000',
-//    font_size: 30,
-//    font:'Verdana'
-//  },
-//  computed: {
-//            // 计算属性的 getter
-//            reversedMessage: function () {
-//                var c=document.getElementById("myCanvas");
-//                var ctx=c.getContext("2d");
-//                ctx.fillStyle="#FF0000";
-//                ctx.fillRect(0,0,300,300);
-//                ctx.fillStyle="#FFFF00";
-//                ctx.font=this.font_size + "px " + this.font;
-//                ctx.textAlign="center";
-//                ctx.fillText(this.text,150,150);
-//                return this.font_size + " " + this.font;
-//            }
-//        }
-//});
+//
 //        \App\Http\Models\Admin\User::create([
 //            'name' => 'asd',
 //            'email' => 'asd@asd.com',
--- resources/views/home/font.blade.php
+++ resources/views/home/font.blade.php
@@ -1,159 +1,123 @@
 @extends('layouts.app')
 
 @section('css')
-    <style>
-        /*.btn-success {*/
-            /*margin-top: 30px;*/
-        /*}*/
-
-        /*.btn-primary {*/
-            /*margin-top: 30px;*/
-            /*font-family: "Adobe Arabic";*/
-        /*}*/
-    </style>
 @endsection
 
 @section('content')
-    {{--<blockquote style="background: snow;margin-bottom: 50px">--}}
-    {{--<p>To make life more beautiful, please use the art icon</p>--}}
-    {{--<footer>Someone famous in <cite title="Source Title">bingoicon</cite></footer>--}}
-    {{--</blockquote>--}}
-    <div class="container" style="display:table;height:100%;">
-        <div class="row" style="display: table-cell;position: relative; border: 1px solid#dddddd;
-   vertical-align: middle;width: 800px;height: 400px;text-align: center">
-            <div class="container  " style="position: absolute;left: 0;top: 0;width: 100%;height: 50px;line-height: 50px;background: #f5f5f5;
-    border-bottom: 1px solid#dddddd;
-">
-                <span style="float: left;color: #5e5e5e;font-size: 20px;">控制输入台</span>
-            </div>
-            <form class="form-inline" action="#" style="margin-top: 20px">
-                <textarea class="form-control" rows="3" id="demo"
-                          style="width: 720px;height: 250px;display: table-cell;"
-                          placeholder="选好字体，背景色，点击提交即可生成">美团</textarea>
-                <div class="form-group" style="margin-top: 10px">
-                    <select class="selectpicker form-control" id="family" style="width: 120px">
-                        @foreach($font as $v)
-                            <option {{ $loop->first ? 'selected' : '' }} style="font-family: {{$v->font_family}}">{{$v->font}}</option>
-                        @endforeach
-                    </select>
-                    <label for="exampleInputName2">字体颜色</label>
-                    <input type="text" value="#ffffff" class="form-control" id="color" placeholder="字体颜色" style="width:120px">
-                    <label for="exampleInputName2">字体大小</label>
-                    <input type="text" value="20" class="form-control" id="sssss" placeholder="字体大小" style="width:120px">
-                    <label for="exampleInputName2">背景色</label>
-                    <input type="text" value="#06c1b0" class="form-control " id="background" placeholder="背景色" style="width: 120px">
-                    <input type="submit" class="form-control" id="submit" value="提交"
-                           style="background:#2ab27b;color: #dddddd">
-                </div>
-            </form>
+    <div class="col-12 col-sm-12 col-md-6 form-group">
+        <div class="center-block" style="width: 300px; height: 300px">
+            <canvas id="draw" style="width: 300px; height: 300px; border-radius: 10px"></canvas>
         </div>
     </div>
-    <div id="div1" style="margin:20px auto 0;width:50px;
-    height:50px;">
-        <canvas id="drawing" style="display: none;" width="50px" height="50px">A Drawing of something</canvas>
-        <canvas id="canvas" style="display: none;" width="1024px" height="1024px">A Drawing of something</canvas>
-        <a download="" class="btn btn-info"
-           style="width:50px;display:none;cursor: pointer;margin:20px auto 0;margin-right: 10px" id="download">下载</a>
+
+    <div class="col-12 col-sm-12 col-md-6">
+        <form class="form-inline">
+            <div class="form-group">
+                <label for="backgroundColor" class="mr-2">背景色</label>
+                <input type="text" id="backgroundColor" class="form-control" placeholder="图标背景色" v-model="backgroundColor">
+            </div>
+
+            <div class="form-group">
+                <label for="text" class="mr-2">文字</label>
+                <input type="text" id="text" class="form-control" placeholder="图标中的文字" v-model="text">
+            </div>
+
+            <div class="form-group">
+                <label for="font_family" class="mr-2">文字颜色</label>
+                <input type="text" id="font_family" class="form-control" placeholder="文字颜色" v-model="textColor">
+            </div>
+
+            <div class="form-group">
+                <label for="font_size" class="mr-2">文字大小</label>
+                <input type="text" id="font_size" class="form-control" placeholder="文字大小" v-model="font_size">
+            </div>
+
+            <div class="form-group">
+                <label for="text_x" class="mr-2">文字中心点位置</label>
+                <input type="text" id="text" class="text_x" placeholder="文字中心点位置" v-model="text_x">
+                <input type="text" id="text" class="text_y" placeholder="文字中心点位置" v-model="text_y">
+            </div>
+        </form>
     </div>
 
+    <div class="col-12 col-sm-12 col-md-6">
+        <button @click="d">
+            asd
+        </button>
+    </div>
 @endsection
 
 @section('js')
     <script>
-        window.onload = function () {
-            var button = document.getElementById('submit');
-            //记录点击
-            var index = 0;
-            //select
-            var select = document.getElementById('family');
-            select.onchange = function () {
-                var index = select.selectedIndex; // 选中索引
-                var text = select.options[index].style.fontFamily; // 选中文字样式
-                select.style.fontFamily = text;
-            }
-            button.onclick = function () {
-                event.preventDefault();
-                index++;
-                var drawing = document.getElementById("drawing");
-                var canvas = document.getElementById("canvas");
-                var download = document.getElementById("download");
-                var div = document.getElementById('div1');
-                var myDate = new Date();
-                //图片名
-                var fileName = myDate.toLocaleTimeString();
-                if (drawing.getContext) {
-                    //取得绘图上下文对象的引用，“2d”是取得2D上下文对象
-                    var context = drawing.getContext("2d");
-                    var background = document.getElementById('background').value;
-                    var sizes = document.getElementById('sssss').value;
-                    var family = document.getElementById('family').value;
-                    var color = document.getElementById('color').value;
-                    var demo = document.getElementById('demo').value;
-                    if (index > 1) {
-                        //防止重复
-                        div.removeChild(div.childNodes[0]);
-                    }
-                    context.fillStyle = background;
-                    context.fillRect(0, 0, 50, 50);
-                    context.font = sizes + 'px ' + family;
-                    context.fillStyle = color;
-                    context.textAlign = "center";
-                    context.fillText(demo, 25, 25 + sizes / 3, 100);
-                    //制作边框
-                    context.lineWidth = 3;
-                    context.moveTo(0, 0);
-                    context.lineTo(50, 0);
-                    context.lineTo(50, 50);
-                    context.lineTo(0, 50);
-                    context.lineTo(0, 0);
-                    context.closePath();//闭合路径
-                    context.strokeStyle = '#dddddd';
-                    context.stroke();
-                    //取得图像的数据URI
-                    var imgURI = drawing.toDataURL("image/png");
-                    //显示图像
-                    var image = document.createElement("img");
-                    image.src = imgURI;
-                    div.insertBefore(image, drawing);
-                    var con = canvas.getContext("2d");
-                    con.fillStyle = background;
-                    con.fillRect(0, 0, 1024, 1024);
-                    con.font = 1024 / 50 * sizes + 'px ' + family;
-                    con.fillStyle = color;
-                    con.textAlign = "center";
-                    con.fillText(demo, 512, 512 + 1024 / 50 * sizes / 3, 1024 / 50 * 100);
-                    con.lineWidth = 3;
-                    con.moveTo(0, 0);
-                    con.lineTo(1024, 0);
-                    con.lineTo(1024, 1024);
-                    con.lineTo(0, 1024);
-                    con.lineTo(0, 0);
-                    con.closePath();//闭合路径
-                    con.strokeStyle = '#dddddd';
-                    con.stroke();
-                    //取得图像的数据URI
-                    var imgURI1 = canvas.toDataURL("image1/png");
-                    //显示图像
-                    var image1 = document.createElement("img");
-                    image1.src = imgURI1;
-                    if (image1) {
-                        download.style.display = "block";
-                        download.onclick = function () {
-                            download.setAttribute('href', imgURI1);
-                            download.setAttribute('download', fileName);
-                        }
-                    }
-                } else {
-                    if (image1) {
-                        download.style.display = "block";
-                        download.onclick = function () {
-                            download.setAttribute('href', imgURI1);
-                            download.setAttribute('download', fileName);
-                        }
-                    }
-                }
+        new Vue({
+            el: '#app',
+            mounted: function () {
+                this.drawc();
+            },
+            data: {
+                text: 'f',
+                backgroundColor: '#3b5998',
+                font_size: 200,
+                font_family: 'Verdana',
+                textColor: '#ffffff',
+                text_x: 150,
+                text_y: 150 ,
+            },
+            methods: {
+                drawc: function () {
+                    c = draw;
+                    ctx = draw.getContext("2d");
 
+                    ctx.fillStyle = this.backgroundColor;
+                    ctx.fillRect(0, 0, 300, 300);
+
+                    ctx.fillStyle = this.textColor;
+                    ctx.font = this.font_size + "px " + this.font_family;
+
+                    ctx.textAlign = "center";
+                    ctx.fillText(this.text, this.text_x, this.text_y);
+                },
+                d: function () {
+                    // var $form = $('<form method="GET"></form>');
+                    // $form.attr('action', draw.toDataURL("image/png"));
+                    // $form.appendTo($('body'));
+                    // $form.submit();
+                    // c = draw;
+                    // url = c.toDataURL("image/png");
+                    // download.style.display = "block";
+                    //
+                    // var myDate = new Date();
+                    // //图片名
+                    // var fileName = myDate.toLocaleTimeString();
+                    // download.onclick = function () {
+                    //     download.setAttribute('href', url);
+                    //     download.setAttribute('download', fileName);
+                    // }
+                }
+            },
+            watch: {
+                text:function () {
+                    this.drawc();
+                },
+                backgroundColor:function () {
+                    this.drawc();
+                },
+                font_size:function () {
+                    this.drawc();
+                },
+                font_family:function () {
+                    this.drawc();
+                },
+                textColor:function () {
+                    this.drawc();
+                },
+                text_x:function () {
+                    this.drawc();
+                },
+                text_y:function () {
+                    this.drawc();
+                },
             }
-        }
+        });
     </script>
 @endsection

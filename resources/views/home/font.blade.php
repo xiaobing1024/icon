ad0a640eca307631554ffc5de0758f66d783ab47
zxb <1030075254@qq.com>
2018-03-19 21:04:33 +0800

    no message

--- resources/views/home/font.blade.php
+++ resources/views/home/font.blade.php
@@ -5,45 +5,91 @@
 
 @section('content')
     <div class="col-12 col-sm-12 col-md-6 form-group">
-        <div class="center-block" style="width: 300px; height: 300px">
-            <canvas id="draw" style="width: 300px; height: 300px; border-radius: 10px"></canvas>
+        <div class="text-center" width="256" height="256">
+            <canvas id="draw" width="256" height="256"></canvas>
         </div>
     </div>
 
     <div class="col-12 col-sm-12 col-md-6">
-        <form class="form-inline">
-            <div class="form-group">
-                <label for="backgroundColor" class="mr-2">背景色</label>
-                <input type="text" id="backgroundColor" class="form-control" placeholder="图标背景色" v-model="backgroundColor">
+        <form>
+            <div class="form-group row">
+                <label for="backgroundColor" class="col-xl-2 col-form-label">背景色</label>
+                <div class="col-xl-4">
+                    <input type="text" id="backgroundColor" class="form-control" placeholder="图标背景色"
+                           v-model="backgroundColor">
+                </div>
+
+                <label for="textColor" class="col-xl-2 col-form-label">文字颜色</label>
+                <div class="col-xl-4">
+                    <input type="text" id="textColor" class="form-control" placeholder="文字颜色" v-model="textColor">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="text" class="mr-2">文字</label>
-                <input type="text" id="text" class="form-control" placeholder="图标中的文字" v-model="text">
+            <div class="form-group row">
+                <label for="text" class="col-xl-2 col-form-label">文字</label>
+                <div class="col-xl-4">
+                    <input type="text" id="text" class="form-control" placeholder="图标中的文字" v-model="text">
+                </div>
+
+                <label for="font_family" class="col-xl-2 col-form-label">字体</label>
+                <div class="col-xl-4">
+                    <input type="text" id="font_family" class="form-control" placeholder="字体" v-model="font_family">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="font_family" class="mr-2">文字颜色</label>
-                <input type="text" id="font_family" class="form-control" placeholder="文字颜色" v-model="textColor">
+            <div class="form-group row">
+                <label for="font_size" class="col-xl-2 col-form-label">字体大小</label>
+                <div class="col-xl-4">
+                    <input type="number" id="font_size" class="form-control" placeholder="字体大小 单位px"
+                           v-model="font_size">
+                </div>
+
+                <label for="font_weight" class="col-xl-2 col-form-label">字体粗细</label>
+                <div class="col-xl-4">
+                    <select name="font_weight" id="font_weight" class="form-control" v-model="font_weight">
+                        <option value="normal">normal</option>
+                        <option value="bold">bold</option>
+                        <option value="bolder">bolder</option>
+                        <option value="lighter">lighter</option>
+                        <option value="100">100</option>
+                        <option value="200">200</option>
+                        <option value="300">300</option>
+                        <option value="400">400</option>
+                        <option value="500">500</option>
+                        <option value="600">600</option>
+                        <option value="700">700</option>
+                        <option value="800">800</option>
+                        <option value="900">900</option>
+                    </select>
+                    {{--<input type="text" id="font_weight" class="form-control" placeholder="字体粗细" v-model="font_weight">--}}
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="font_size" class="mr-2">文字大小</label>
-                <input type="text" id="font_size" class="form-control" placeholder="文字大小" v-model="font_size">
+
+            <div class="form-group row">
+                <label for="text_x" class="col-xl-2 col-form-label">中心-X</label>
+                <div class="col-xl-4">
+                    <input type="number" id="text_x" class="form-control" placeholder="128为中点" v-model="text_x">
+                </div>
+
+                <label for="text_y" class="col-xl-2 col-form-label">中心-Y</label>
+                <div class="col-xl-4">
+                    <input type="number" id="text_y" class="form-control" placeholder="128为中点" v-model="text_y">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="text_x" class="mr-2">文字中心点位置</label>
-                <input type="text" id="text" class="text_x" placeholder="文字中心点位置" v-model="text_x">
-                <input type="text" id="text" class="text_y" placeholder="文字中心点位置" v-model="text_y">
+            <div class="form-group row">
+                <div class="col-12 mt-3">
+                    <a class="btn btn-block btn-outline-success btn-lg" id="go" type="button">
+                        <i class="fa fa-picture-o"></i> 下载图片
+                    </a>
+                </div>
             </div>
         </form>
     </div>
 
-    <div class="col-12 col-sm-12 col-md-6">
-        <button @click="d">
-            asd
-        </button>
+    <div class="text-center" width="1024" height="1024" style="display: none">
+        <canvas id="drawcopy" width="1024" height="1024"></canvas>
     </div>
 @endsection
 
@@ -57,64 +103,67 @@
             data: {
                 text: 'f',
                 backgroundColor: '#3b5998',
-                font_size: 200,
+                font_size: 300,
                 font_family: 'Verdana',
+                font_weight: 'normal',
                 textColor: '#ffffff',
-                text_x: 150,
-                text_y: 150 ,
+                text_x: 148,
+                text_y: 160,
             },
             methods: {
                 drawc: function () {
-                    c = draw;
                     ctx = draw.getContext("2d");
 
                     ctx.fillStyle = this.backgroundColor;
-                    ctx.fillRect(0, 0, 300, 300);
+                    ctx.fillRect(0, 0, 256, 256);
 
                     ctx.fillStyle = this.textColor;
-                    ctx.font = this.font_size + "px " + this.font_family;
+                    ctx.font = this.font_weight + " " + this.font_size + "px " + this.font_family;
 
+                    ctx.textBaseline = "middle";
                     ctx.textAlign = "center";
                     ctx.fillText(this.text, this.text_x, this.text_y);
-                },
-                d: function () {
-                    // var $form = $('<form method="GET"></form>');
-                    // $form.attr('action', draw.toDataURL("image/png"));
-                    // $form.appendTo($('body'));
-                    // $form.submit();
-                    // c = draw;
-                    // url = c.toDataURL("image/png");
-                    // download.style.display = "block";
-                    //
-                    // var myDate = new Date();
-                    // //图片名
-                    // var fileName = myDate.toLocaleTimeString();
-                    // download.onclick = function () {
-                    //     download.setAttribute('href', url);
-                    //     download.setAttribute('download', fileName);
-                    // }
+
+
+                    ctx = drawcopy.getContext("2d");
+
+                    ctx.fillStyle = this.backgroundColor;
+                    ctx.fillRect(0, 0, 1024, 1024);
+
+                    ctx.fillStyle = this.textColor;
+                    ctx.font = this.font_weight + " " + (this.font_size * 4) + "px " + this.font_family;
+
+                    ctx.textBaseline = "middle";
+                    ctx.textAlign = "center";
+                    ctx.fillText(this.text, this.text_x * 4, this.text_y * 4);
+
+                    go.href = drawcopy.toDataURL("image/png");
+                    go.download = "icon.png";
                 }
             },
             watch: {
-                text:function () {
+                text: function () {
                     this.drawc();
                 },
-                backgroundColor:function () {
+                backgroundColor: function () {
                     this.drawc();
                 },
-                font_size:function () {
+                font_size: function () {
                     this.drawc();
                 },
-                font_family:function () {
+                font_family: function () {
                     this.drawc();
                 },
-                textColor:function () {
+                font_weight: function () {
                     this.drawc();
                 },
-                text_x:function () {
+                textColor: function () {
                     this.drawc();
                 },
-                text_y:function () {
+                text_x: function () {
+                    this.drawc();
+                },
+                text_y: function () {
                     this.drawc();
                 },
             }

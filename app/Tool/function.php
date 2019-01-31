c7beccffb30746b333095ca1aee1450afe9b56a9
zxb <1030075254@qq.com>
2019-01-31 18:46:47 +0800

    xxx

--- app/Tool/function.php
+++ app/Tool/function.php
@@ -1,9 +1,14 @@
 <?php
 
 if (! function_exists('cache_map')) {
-
     function cache_map($k, $d = null) {
         $maps = cache('map', []);
         return array_key_exists($k, $maps) ? $maps[$k] : $d;
+    }
+}
+
+if (! function_exists('dda')) {
+    function dda($a) {
+        dd($a->toArray());
     }
 }
